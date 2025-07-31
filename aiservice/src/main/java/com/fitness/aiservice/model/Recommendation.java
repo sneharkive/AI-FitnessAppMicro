22250a51be2deb5c05d2a122700dc1ca6002aab2
Sneha <sneharoy90737@gmail.com>
2025-07-31 21:21:55 +0530

    Basic aiservice set up

---
+++ aiservice/src/main/java/com/fitness/aiservice/model/Recommendation.java
@@ -0,0 +1,35 @@
+package com.fitness.aiservice.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "recommendations")
+public class Recommendation {
+
+  @Id
+  private String id;
+  private String activityId;
+  private String userId;
+  private String activityType;
+  private String recommendation;
+  private List<String> improvement;
+  private List<String> suggestions;
+  private List<String> safety;
+
+  @CreatedDate
+  private LocalDateTime createAt;
+
+}
